331853767957e0556d24974a9ece3ee8216c3614
arrownegra <89271120+arrownegra@users.noreply.github.com>
2021-08-27 16:38:33 +0100

    ok

---
+++ plugin.video.arrownegra.play.tv/uservar.py
@@ -0,0 +1,16 @@
+'''
+----------Edit required---------
+Set pluginid to same name as addon id in addon.xml
+'''
+pluginid = 'plugin.video.arrownegra.play.tv'
+
+
+''' 
+----------Edit required---------
+For online xml for sources, edit the host to the url of the file host='http://something.com/sources.xml'
+For local xml file, edit the host to local host='local' and call the file 'sources.xml' and place in the folder
+please see sample.xml for layout
+on line source can be either https or http
+'''
+
+host='local'
